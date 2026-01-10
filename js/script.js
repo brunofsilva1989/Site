aaf56ebc5361a4525e721b1fe75778a7b08aa986
brunofsilva1989 <brunofsilva1989@gmail.com>
2026-01-10 18:20:37 -0300

    Novas mudanças no Site

--- js/script.js
+++ js/script.js
@@ -25,3 +25,34 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.2 });
 
 document.querySelectorAll('.fade-section').forEach(sec => observer.observe(sec));
+
+/*Splash Screen*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const splash = document.getElementById("splash");
+  if (!splash) return;
+
+  // trava scroll enquanto splash aparece
+  const prevOverflow = document.body.style.overflow;
+  document.body.style.overflow = "hidden";
+
+  // tempo do splash (3 a 5 segundos)
+  const SPLASH_TIME = 3500; // 3000 a 5000
+
+  // permite "pular" clicando
+  splash.addEventListener("click", hideSplash);
+
+  // fecha automático
+  const t = setTimeout(hideSplash, SPLASH_TIME);
+
+  function hideSplash() {
+    clearTimeout(t);
+    splash.classList.add("hide");
+
+    // depois da transição, remove de vez e libera scroll
+    setTimeout(() => {
+      splash.remove();
+      document.body.style.overflow = prevOverflow || "";
+    }, 500);
+  }
+});
